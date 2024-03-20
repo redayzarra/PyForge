a097e610d672cf40b9095d08b1995a98bf5b2f4b
ReDay Zarra <redayzarra@gmail.com>
2024-03-19 23:14:03 -0400

    Getting hardcoded example to run

---
+++ rz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace rz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter an expression (or press Enter to exit):");
+                var line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                    return;
+                
+                if (line == "4 + 5")
+                    Console.WriteLine("7");
+                else
+                {
+                    Console.WriteLine("ERROR: Invalid Expression");
+                }
+            }
+        }
+    }
+}
