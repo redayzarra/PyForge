f4c55bb6168206cf5186d8c0008c302c4d82516c
ReDay Zarra <redayzarra@gmail.com>
2024-03-21 06:19:05 -0400

    Lexer can recognize numbers and basic math operators

--- rz/Program.cs
+++ rz/Program.cs
@@ -28,20 +28,32 @@ namespace rz
     }
 
     enum SyntaxKind
-    {}
+    {
+        NumberToken,
+        WhitespaceToken,
+        PlusToken,
+        MinusToken,
+        StarToken,
+        SlashToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
+        BadToken,
+    }
 
     class SyntaxToken
     {
-        public SyntaxToken(SyntaxKind kind, int position, string text)
+        public SyntaxToken(SyntaxKind kind, int position, string text, object? value)
         {
             Kind = kind;
             Position = position;
             Text = text;
+            Value = value;
         }
 
         public SyntaxKind Kind { get; }
         public int Position { get; }
         public string Text { get; }
+        public object? Value { get; }
     }
 
     class Lexer
@@ -62,7 +74,39 @@ namespace rz
         }
 
         public SyntaxToken NextToken()
-        {} 
+        {
+            if (_position >= _text.Length)
+                return new SyntaxToken(SyntaxKind.BadToken, _position, "\0", null);
+
+            if (char.IsDigit(Current))
+            {
+                var start = _position;
+                
+                while (char.IsDigit(Current))
+                    Next();
+                
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                int.TryParse(text, out var value);
+                return new SyntaxToken(SyntaxKind.NumberToken, start, text, value);
+            }
+
+            if (char.IsWhiteSpace(Current))
+                Next();
+
+            var tokenKind = Current switch
+            {
+                '+' => SyntaxKind.PlusToken,
+                '-' => SyntaxKind.MinusToken,
+                '*' => SyntaxKind.StarToken,
+                '/' => SyntaxKind.SlashToken,
+                '(' => SyntaxKind.OpenParenthesisToken,
+                ')' => SyntaxKind.CloseParenthesisToken,
+                _ => SyntaxKind.BadToken,
+            };
+            
+            return new SyntaxToken(tokenKind, _position++, _text.Substring(_position - 1, 1), null);
+        } 
     }
 }
 
