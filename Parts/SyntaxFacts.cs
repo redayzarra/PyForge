1cd523ba37b8bff5e8283908312780274e3c9306
ReDay Zarra <redayzarra@gmail.com>
2024-04-07 23:22:43 -0400

    Adding unary precedence to syntax facts

--- Parts/SyntaxFacts.cs
+++ Parts/SyntaxFacts.cs
@@ -2,6 +2,21 @@ namespace Compiler.Parts
 {
     internal static class SyntaxFacts
     {
+        // Get the precedence of the unary operators (e.g. -1 + 2)
+        internal static int GetUnaryOperator(SyntaxKind kind)
+        {
+            switch (kind)
+            {
+                case SyntaxKind.PlusToken:
+                case SyntaxKind.MinusToken:
+                    return 3;
+                
+                default:
+                    return 0;
+            }
+        }
+
+        // Get the precedence of operators (PEMDAS)
         internal static int GetBinaryOperator(SyntaxKind kind)
         {
             switch (kind)
