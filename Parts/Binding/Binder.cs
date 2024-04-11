2372b3188851ced3486dbafcd23780968b7ea341
ReDay Zarra <redayzarra@gmail.com>
2024-04-10 23:38:25 -0400

    Adding bound binary expression class

--- Parts/Binding/Binder.cs
+++ Parts/Binding/Binder.cs
@@ -49,4 +49,29 @@ namespace Compiler.Parts.Binding
         public BoundExpression Operand { get; }
 
     }
+
+    internal enum BoundBinaryOperatorKind
+    {
+        Addition, 
+        Subtraction, 
+        Multiplication, 
+        Division
+    }
+
+    internal sealed class BoundBinaryExpression : BoundExpression
+    {
+        public BoundBinaryExpression(BoundExpression left, BoundBinaryOperatorKind operatorKind, BoundExpression right)
+        {
+            Left = left;
+            OperatorKind = operatorKind;
+            Right = right;
+        }
+
+        public override Type Type => Left.Type;
+        public override BoundNodeKind Kind => BoundNodeKind.UnaryExpression;
+
+        public BoundExpression Left { get; }
+        public BoundBinaryOperatorKind OperatorKind { get; }
+        public BoundExpression Right { get; }
+    }
 }
