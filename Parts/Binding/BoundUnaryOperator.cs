0f819b2703cad7a035c36b9aea58d2d0edd56431
ReDay Zarra <redayzarra@gmail.com>
2024-04-16 08:42:38 -0400

    Creating bound unary operator kind

---
+++ Parts/Binding/BoundUnaryOperator.cs
@@ -0,0 +1,44 @@
+using Compiler.Parts.Syntax;
+
+namespace Compiler.Parts.Binding
+{
+    internal sealed class BoundUnaryOperator
+    {
+        private BoundUnaryOperator(SyntaxKind syntaxKind, BoundUnaryOperatorKind kind, Type operandType)
+        : this (syntaxKind, kind, operandType, operandType)
+        {
+        }
+
+        private BoundUnaryOperator(SyntaxKind syntaxKind, BoundUnaryOperatorKind kind, Type operandType, Type resultType)
+        {
+            SyntaxKind = syntaxKind;
+            Kind = kind;
+            OperandType = operandType;
+            ResultType = resultType;
+        }
+
+        public SyntaxKind SyntaxKind { get; }
+        public BoundUnaryOperatorKind Kind { get; }
+        public Type OperandType { get; }
+        public Type ResultType { get; }
+
+        private static BoundUnaryOperator[] _operators = 
+        {
+            new BoundUnaryOperator(SyntaxKind.AndKeyword, BoundUnaryOperatorKind.LogicalNegation, typeof(bool)),
+            new BoundUnaryOperator(SyntaxKind.PlusToken, BoundUnaryOperatorKind.Identity, typeof(int)),
+            new BoundUnaryOperator(SyntaxKind.MinusToken, BoundUnaryOperatorKind.LogicalNegation, typeof(int)),
+        };
+
+        public static BoundUnaryOperator? Bind(SyntaxKind syntaxKind, Type operandType)
+        {
+            foreach (var op in _operators)
+            {
+                if (op.SyntaxKind == syntaxKind && op.OperandType == operandType)
+                    return op; 
+            }
+        
+            return null;
+        }
+    }
+    
+}
