6fdec453351706ea22ceda8a3fe8bdd9f3d7e17a
ReDay Zarra <redayzarra@gmail.com>
2024-04-25 17:08:21 -0400

    Refactoring and improving testing suite

--- Tests/Parts/Syntax/LexerTest.cs
+++ Tests/Parts/Syntax/LexerTest.cs
@@ -4,12 +4,14 @@ namespace Compiler.Tests.Parts.Syntax;
 
 public class LexerTest
 {
+    private static readonly List<(SyntaxKind kind, string text)> AllTokens = GetTokens().ToList();
+    private static readonly List<(SyntaxKind kind, string text)> AllSeparators = GetSeparators().ToList();
+
     [Theory]
     [MemberData(nameof(GetTokensData))]
     public void Lexes_Token(SyntaxKind kind, string text)
     {
         var tokens = SyntaxTree.ParseTokens(text);
-
         var token = Assert.Single(tokens);
         Assert.Equal(kind, token.Kind);
         Assert.Equal(text, token.Text);
@@ -23,7 +25,6 @@ public class LexerTest
         {
             var text = firstText + separator + secondText;
             var tokens = SyntaxTree.ParseTokens(text).ToArray();
-
             Assert.Equal(2, tokens.Length);
             Assert.Equal(firstKind, tokens[0].Kind);
             Assert.Equal(firstText, tokens[0].Text);
@@ -31,111 +32,93 @@ public class LexerTest
             Assert.Equal(secondText, tokens[1].Text);
         }
 
-        // Test with the appropriate separators
         if (RequiresSeparator(firstKind, secondKind))
         {
             TestTokenPair(" ");
             TestTokenPair("\t");
         }
-        else // Test without any separator
+        else
         {
             TestTokenPair("");
         }
     }
 
-    public static IEnumerable<object[]> GetTokensData()
-    {
-        foreach (var token in GetTokens())
-            yield return new object[] {token.kind, token.text};
-    }
+    public static IEnumerable<object[]> GetTokensData() => AllTokens.Select(t => new object[] { t.kind, t.text });
 
-    public static IEnumerable<object[]> GetTokenPairsData()
-    {
-        foreach (var token in GetTokenPairs())
-            yield return new object[] {token.firstKind, token.firstText, token.secondKind, token.secondText};
-    }
+    public static IEnumerable<object[]> GetTokenPairsData() =>
+        from first in AllTokens
+        from second in AllTokens
+        where !RequiresSeparator(first.kind, second.kind)
+        select new object[] { first.kind, first.text, second.kind, second.text };
 
     private static IEnumerable<(SyntaxKind kind, string text)> GetTokens()
-        {
-            // Literal Tokens
-            yield return (SyntaxKind.NumberToken, "123");
-            yield return (SyntaxKind.IdentifierToken, "a");
-            yield return (SyntaxKind.IdentifierToken, "reday");
-            yield return (SyntaxKind.IdentifierToken, "loves");
-            yield return (SyntaxKind.IdentifierToken, "ashley");
-
-            // Operator Tokens
-            yield return (SyntaxKind.PlusToken, "+");
-            yield return (SyntaxKind.MinusToken, "-");
-            yield return (SyntaxKind.StarToken, "*");
-            yield return (SyntaxKind.SlashToken, "/");
-            yield return (SyntaxKind.EqualsEqualsToken, "==");
-            yield return (SyntaxKind.NotEqualsToken, "!=");
-            yield return (SyntaxKind.EqualsToken, "=");
-
-            // Punctuation Tokens
-            yield return (SyntaxKind.OpenParenthesisToken, "(");
-            yield return (SyntaxKind.CloseParenthesisToken, ")");
-
-            // Keyword Tokens
-            yield return (SyntaxKind.TrueKeyword, "True");
-            yield return (SyntaxKind.FalseKeyword, "False");
-            yield return (SyntaxKind.NotKeyword, "not");
-            yield return (SyntaxKind.IsKeyword, "is");
-            yield return (SyntaxKind.AndKeyword, "and");
-            yield return (SyntaxKind.OrKeyword, "or");
-
-            // Whitespace Tokens
-            // yield return (SyntaxKind.WhitespaceToken, " ");
-            // yield return (SyntaxKind.WhitespaceToken, "   ");
-            // yield return (SyntaxKind.WhitespaceToken, "\t");
-            // yield return (SyntaxKind.WhitespaceToken, "\n");
-            // yield return (SyntaxKind.WhitespaceToken, "\r");
-            // yield return (SyntaxKind.WhitespaceToken, "\r\n");
-            // yield return (SyntaxKind.WhitespaceToken, "\r\t");
-        }
-
-        private static bool RequiresSeparator(SyntaxKind firstKind, SyntaxKind secondKind)
-        {
-            switch (firstKind)
-            {
-                case SyntaxKind.IdentifierToken:
-                    return true; // Identifiers generally need separation from anything that might follow
-
-                case SyntaxKind.NumberToken:
-                    return secondKind == SyntaxKind.NumberToken || secondKind == SyntaxKind.IdentifierToken;
-
-                case SyntaxKind.NotKeyword:
-                    return true; // "not" should be separated from anything that follows to prevent merging
-
-                case SyntaxKind.EqualsToken:
-                case SyntaxKind.EqualsEqualsToken:
-                    return secondKind == SyntaxKind.EqualsToken || secondKind == SyntaxKind.EqualsEqualsToken;
-
-                default:
-                    // Check if the token kind is a keyword
-                    return IsKeyword(firstKind);
-            }
-        }
-
-        private static bool IsKeyword(SyntaxKind kind)
-        {
-            // Use a set to identify if a token kind is a keyword
-            return Enum.GetName(typeof(SyntaxKind), kind)?.EndsWith("Keyword") ?? false;
-        }
-
-    private static IEnumerable<(SyntaxKind firstKind, string firstText, SyntaxKind secondKind, string secondText)> GetTokenPairs()
     {
-        // Retrieves all tokens once and reuses the list to avoid multiple enumerations
-        var allTokens = GetTokens().ToList();
+        // Literal Tokens
+        yield return (SyntaxKind.NumberToken, "123");
+        yield return (SyntaxKind.IdentifierToken, "a");
+        yield return (SyntaxKind.IdentifierToken, "reday");
+        yield return (SyntaxKind.IdentifierToken, "loves");
+        yield return (SyntaxKind.IdentifierToken, "ashley");
 
-        foreach (var firstToken in allTokens)
+        // Operator Tokens
+        yield return (SyntaxKind.PlusToken, "+");
+        yield return (SyntaxKind.MinusToken, "-");
+        yield return (SyntaxKind.StarToken, "*");
+        yield return (SyntaxKind.SlashToken, "/");
+        yield return (SyntaxKind.EqualsEqualsToken, "==");
+        yield return (SyntaxKind.NotEqualsToken, "!=");
+        yield return (SyntaxKind.EqualsToken, "=");
+
+        // Punctuation Tokens
+        yield return (SyntaxKind.OpenParenthesisToken, "(");
+        yield return (SyntaxKind.CloseParenthesisToken, ")");
+
+        // Keyword Tokens
+        yield return (SyntaxKind.TrueKeyword, "True");
+        yield return (SyntaxKind.FalseKeyword, "False");
+        yield return (SyntaxKind.NotKeyword, "not");
+        yield return (SyntaxKind.IsKeyword, "is");
+        yield return (SyntaxKind.AndKeyword, "and");
+        yield return (SyntaxKind.OrKeyword, "or");
+    }
+
+    private static IEnumerable<(SyntaxKind kind, string text)> GetSeparators()
+    {
+        // Whitespace Tokens
+        yield return (SyntaxKind.WhitespaceToken, " ");
+        yield return (SyntaxKind.WhitespaceToken, "   ");
+        yield return (SyntaxKind.WhitespaceToken, "\t");
+        yield return (SyntaxKind.WhitespaceToken, "\n");
+        yield return (SyntaxKind.WhitespaceToken, "\r");
+        yield return (SyntaxKind.WhitespaceToken, "\r\n");
+        yield return (SyntaxKind.WhitespaceToken, "\r\t");
+    }
+
+    private static bool RequiresSeparator(SyntaxKind firstKind, SyntaxKind secondKind)
+    {
+        switch (firstKind)
         {
-            foreach (var secondToken in allTokens)
-            {
-                if (!RequiresSeparator(firstToken.kind, secondToken.kind))
-                    yield return (firstToken.kind, firstToken.text, secondToken.kind, secondToken.text);
-            }
+            case SyntaxKind.IdentifierToken:
+            case SyntaxKind.NotKeyword:
+                return true; // These kinds should be separated from anything that follows
+
+            case SyntaxKind.NumberToken:
+                return secondKind == SyntaxKind.NumberToken || secondKind == SyntaxKind.IdentifierToken;
+
+            case SyntaxKind.EqualsToken:
+            case SyntaxKind.EqualsEqualsToken:
+                return secondKind == SyntaxKind.EqualsToken || secondKind == SyntaxKind.EqualsEqualsToken;
+
+            default:
+                return IsKeyword(firstKind);
         }
     }
+
+    private static bool IsKeyword(SyntaxKind kind)
+    {
+        // Determine if a token kind is a keyword based on naming convention
+        return Enum.GetName(typeof(SyntaxKind), kind)?.EndsWith("Keyword") ?? false;
+    }
+
+    
 }
