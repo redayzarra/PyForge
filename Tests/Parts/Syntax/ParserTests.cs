3fd43652e432c3901925385a370f9bba676e9f4f
ReDay Zarra <redayzarra@gmail.com>
2024-04-29 06:34:36 -0400

    Adding get binary operator pairs

--- Tests/Parts/Syntax/ParserTests.cs
+++ Tests/Parts/Syntax/ParserTests.cs
@@ -5,10 +5,32 @@ namespace Compiler.Tests.Parts.Syntax;
 public partial class ParserTests
 {
     [Theory]
-    [MemberData(GetBinaryOperatorParis)]
+    [MemberData(nameof(GetBinaryOperatorPairs))]
     public void BinaryExpression_Precedence(SyntaxKind firstOperator, SyntaxKind secondOperator)
     {
-        
+        var firstPrecedence = SyntaxFacts.GetBinaryOperator(firstOperator);
+        var secondPrecedence = SyntaxFacts.GetBinaryOperator(secondOperator);
+        var firstText = SyntaxFacts.GetText(firstOperator);
+        var secondText = SyntaxFacts.GetText(secondOperator);
+
+        var text = $"a {firstText} b {secondText} c";
+
+        if (firstPrecedence >= secondPrecedence)
+        {
+
+        }
+        else 
+        {
+            
+        }
+    }
+
+    public static IEnumerable<object[]> GetBinaryOperatorPairs()
+    {
+        var operators = SyntaxFacts.GetBinaryOperatorKinds().ToList();
+        return from firstOperator in operators
+               from secondOperator in operators
+               select new object[] { firstOperator, secondOperator };
     }
 
 }
