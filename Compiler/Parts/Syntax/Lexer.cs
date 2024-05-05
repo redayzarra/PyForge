8b007c5fef0b1fe8ff89319c0ff8d827a3c154b0
ReDay Zarra <redayzarra@gmail.com>
2024-05-05 07:53:14 -0400

    Changing Lexer back to previous state and refactoring

--- Compiler/Parts/Syntax/Lexer.cs
+++ Compiler/Parts/Syntax/Lexer.cs
@@ -4,12 +4,12 @@ namespace Compiler.Parts.Syntax
     internal sealed class Lexer
     {
         private readonly string _text;
-        private int _position;
+        private readonly DiagnosticBag _diagnostics = new DiagnosticBag();
 
+        private int _position;
         private SyntaxKind _kind;
         private int _start;
-
-        private readonly DiagnosticBag _diagnostics = new DiagnosticBag();
+        private object _value;
 
         public Lexer(string text)
         {
@@ -36,40 +36,13 @@ namespace Compiler.Parts.Syntax
 
         private string ConsumeWhile(Func<char, bool> condition)
         {
-            _start = _position;
+            var start = _position;
             while (condition(Current))
                 Next();
-            return _text.Substring(_start, _position - _start);
+            return _text.Substring(start, _position - start);
         }
 
         public SyntaxToken Lex()
-        {
-            int start = _position;
-
-            // List of functions to check for each type of token
-            var tokenChecks = new List<Func<SyntaxToken?>>()
-            {
-                CheckForWhitespace,
-                CheckForEqualityOperators,
-                CheckForSingleCharacterOperators,
-                CheckForLetters,
-                CheckForDigits,
-                CheckForSpecialCharacters 
-            };
-
-            // Iterate through each check and return the first non-null token found
-            foreach (var check in tokenChecks)
-            {
-                var token = check();
-                if (token != null)
-                    return token;
-            }
-
-            // If no token is found, return a bad token
-            return new SyntaxToken(SyntaxKind.BadToken, start, "", null);
-        }
-
-        private SyntaxToken? CheckForWhitespace()
         {
             if (char.IsWhiteSpace(Current))
             {
@@ -77,34 +50,50 @@ namespace Compiler.Parts.Syntax
                 var whitespace = ConsumeWhile(char.IsWhiteSpace);
                 return new SyntaxToken(SyntaxKind.WhitespaceToken, starting, whitespace, null);
             }
-            return null;
-        }
 
-        private SyntaxToken? CheckForEqualityOperators()
-        {
+            var start = _position;
+
+            // Check for equality operator
             if (Current == '=' && LookAhead == '=')
             {
                 _position += 2; 
-                return new SyntaxToken(SyntaxKind.EqualsEqualsToken, _start, "==", null);
+                return new SyntaxToken(SyntaxKind.EqualsEqualsToken, start, "==", null);
             }
+            
+            // Check for inequality operator
             if (Current == '!' && LookAhead == '=')
             {
                 _position += 2;
-                return new SyntaxToken(SyntaxKind.NotEqualsToken, _start, "!=", null);
+                return new SyntaxToken(SyntaxKind.NotEqualsToken, start, "!=", null);
             }
+
+            // Check for single equals sign, which signifies assignment
             if (Current == '=' && LookAhead != '=')
             {
                 Next();
-                return new SyntaxToken(SyntaxKind.EqualsToken, _start, "=", null);
+                return new SyntaxToken(SyntaxKind.EqualsToken, start, "=", null);
             }
-            return null;
-        }
 
-        private SyntaxToken? CheckForSingleCharacterOperators()
-        {
-            char currentChar = Current;
-            SyntaxKind tokenKind = currentChar switch
+            if (char.IsLetter(Current))
             {
+                var text = ConsumeWhile(char.IsLetter);
+                var kind = SyntaxFacts.GetKeywordKind(text);
+                return new SyntaxToken(kind, start, text, null);
+            }
+
+            if (char.IsDigit(Current))
+            {
+                var text = ConsumeWhile(char.IsDigit);
+                var length = _position - start;  // Calculate the length
+                if (!int.TryParse(text, out var value))
+                    _diagnostics.ReportInvalidNumber(new TextSpan(start, length), _text, typeof(int));
+                return new SyntaxToken(SyntaxKind.NumberToken, start, text, value);
+            }
+
+            var currentChar = Current;  // Save the current character before calling Next()
+            var tokenKind = currentChar switch
+            {
+                '\0' => SyntaxKind.EndOfFileToken,
                 '+' => SyntaxKind.PlusToken,
                 '-' => SyntaxKind.MinusToken,
                 '*' => SyntaxKind.StarToken,
@@ -114,53 +103,12 @@ namespace Compiler.Parts.Syntax
                 _ => SyntaxKind.BadToken,
             };
 
-            if (tokenKind != SyntaxKind.BadToken)
-            {
-                Next(); // Advance the position for single character token
-                return new SyntaxToken(tokenKind, _start, currentChar.ToString(), null);
-            }
+            if (tokenKind == SyntaxKind.BadToken)
+                _diagnostics.ReportBadCharacter(_position, Current);
 
-            return null;
-        }
+            Next(); // Advance the position for single character token
 
-        private SyntaxToken? CheckForLetters()
-        {
-            if (char.IsLetter(Current))
-            {
-                var text = ConsumeWhile(char.IsLetter);
-                var kind = SyntaxFacts.GetKeywordKind(text);
-                return new SyntaxToken(kind, _start, text, null);
-            }
-            return null;
-        }
-
-        private SyntaxToken? CheckForDigits()
-        {
-            if (char.IsDigit(Current))
-            {
-                var text = ConsumeWhile(char.IsDigit);
-                var length = _position - _start;  // Calculate the length
-                if (!int.TryParse(text, out var value))
-                {
-                    _diagnostics.ReportInvalidNumber(new TextSpan(_start, length), _text, typeof(int));
-                    return new SyntaxToken(SyntaxKind.NumberToken, _start, text, null);
-                }
-
-                _kind = SyntaxKind.NumberToken;
-                return new SyntaxToken(_kind, _start, text, value);
-            }
-            return null;
-        }
-
-        private SyntaxToken? CheckForSpecialCharacters()
-        {
-            if (Current == '\0')
-                return new SyntaxToken(SyntaxKind.EndOfFileToken, _position, "\0", null);
-
-            _diagnostics.ReportBadCharacter(_position, Current);
-            var result = new SyntaxToken(SyntaxKind.BadToken, _start, Current.ToString(), null);
-            Next(); // Consume the bad character
-            return result;
+            return new SyntaxToken(tokenKind, start, currentChar.ToString(), null);
         }
     }
 }
